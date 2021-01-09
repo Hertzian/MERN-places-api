d212a636a68b30f57e5eab3dc5d235422c6dc3b9
Lalitus <tallitosan@gmail.com>
2021-01-09 12:35:59 -0600

    handling errors to unsuported routes

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express')
 
 // routes
 const placesRoutes = require('./routes/places-routes')
+const HttpError = require('./models/http-error')
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/api/places', placesRoutes) // /api/places/...
+
+app.use((req, res, next) => {
+  const error = new HttpError('Could not find this route.', 404)
+  throw error
+})
 
 // error handling
 app.use((error, req, res, next) => {
