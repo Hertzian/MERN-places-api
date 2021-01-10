5454ca2873efdd8ef6b099784c736fe314281614
Lalitus <tallitosan@gmail.com>
2021-01-10 17:13:25 -0600

    removed dummy data from placesController

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -7,35 +7,6 @@ const getCoordsForAddress = require('../utils/location')
 const Place = require('../models/PlaceModel')
 const User = require('../models/UserModel')
 
-let DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous skyscrapers is the world!',
-    imageUrl:
-      'https://images.pexels.com/photos/2404843/pexels-photo-2404843.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
-    address: '20 W 34th St, New York, NY 10001',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584,
-    },
-    creator: 'u1',
-  },
-  // {
-  //   id: 'p2',
-  //   title: 'Emp. State Building',
-  //   description: 'One of the most famous skyscrapers is the world!',
-  //   imageUrl:
-  //     'https://images.pexels.com/photos/2404843/pexels-photo-2404843.jpeg?auto=compress&cs=tinysrgb&h=650&w=940',
-  //   address: '20 W 34th St, New York, NY 10001',
-  //   location: {
-  //     lat: 40.7484405,
-  //     lng: -73.9878584,
-  //   },
-  //   creator: 'u2',
-  // },
-]
-
 // @desc    get place by id
 // @route   GET /api/places/:placeId
 // @access  private
