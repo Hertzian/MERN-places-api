47c45dc642386e0e2e9f707c06e5eff4ed58b380
Lalitus <tallitosan@gmail.com>
2021-01-10 02:02:20 -0600

    conecting to mongoAtlas with mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config()
+const mongoose = require('mongoose')
 
 // routes
 const placesRoutes = require('./routes/places-routes')
@@ -30,4 +31,20 @@ app.use((error, req, res, next) => {
     .json({ message: error.message || 'An unknown error occurred!' })
 })
 
-app.listen(5000)
+const PORT = process.env.PORT || 5000
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(
+      PORT,
+      console.log(
+        `Server running in ${process.env.NODE_ENV} on port ${process.env.PORT}`
+      )
+    )
+  })
+  .catch((err) => {
+    console.log(err)
+  })
