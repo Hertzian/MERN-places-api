422a916e0842201a33360da522416e77fe29dea2
Lalitus <tallitosan@gmail.com>
2021-01-10 21:14:56 -0600

    CORS handling

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ const app = express()
 
 app.use(express.json())
 
+// to prevent CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+  next()
+})
+
 app.use('/api/places', placesRoutes)
 app.use('/api/users', usersRoutes)
 
